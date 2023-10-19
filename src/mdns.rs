25c78a76491fd6533a90b47fddd4e53c2e9539ea
rise0chen <rise0chen@163.com>
2023-10-19 02:32:41 +0000

    fix: mdns

--- src/mdns.rs
+++ src/mdns.rs
@@ -3,6 +3,7 @@ use core::time::Duration;
 use futures_util::future::{select, Either};
 use mdns_sd::{ServiceDaemon, ServiceEvent};
 use proto::lebai::multi_devices::DeviceInfo;
+use std::net::IpAddr;
 
 const SERVICE_NAME: &str = "_lebai._tcp.local.";
 
@@ -12,44 +13,42 @@ pub async fn discover_devices(time: f64) -> Result<Vec<DeviceInfo>> {
     let mut devices: Vec<DeviceInfo> = Vec::new();
     let mut task_wait = futures_timer::Delay::new(Duration::from_millis((time * 1000.0) as u64));
     let mut task_recv = receiver.recv_async();
-    loop {
-        match select(task_recv, task_wait).await {
-            Either::Left((res, fut_wait)) => {
-                let res = if let Ok(res) = res { res } else { break };
-                task_wait = fut_wait;
-                task_recv = receiver.recv_async();
+    while let Either::Left((res, fut_wait)) = select(task_recv, task_wait).await {
+        let res = if let Ok(res) = res { res } else { break };
+        task_wait = fut_wait;
+        task_recv = receiver.recv_async();
 
-                match res {
-                    ServiceEvent::ServiceResolved(record) => {
-                        let name = if let Some(x) = record.get_fullname().split(".").nth(0) {
-                            x.to_owned()
-                        } else {
-                            continue;
-                        };
-                        let mac = if let Some(x) = record.get_property_val_str("mac") {
-                            x.to_owned()
-                        } else {
-                            String::new()
-                        };
-                        let mut ip = None;
-                        for addr in record.get_addresses().iter() {
-                            if addr.is_private() {
-                                ip = Some(addr.to_string());
-                                break;
-                            }
-                        }
-                        let ip = if let Some(ip) = ip { ip } else { continue };
-
-                        if let Some(device) = devices.iter_mut().find(|x| x.name == name) {
-                            device.ip = ip;
-                        } else {
-                            devices.push(DeviceInfo { name, mac, ip, online: true })
+        match res {
+            ServiceEvent::ServiceResolved(record) => {
+                let name = if let Some(x) = record.get_fullname().split('.').next() {
+                    x.to_owned()
+                } else {
+                    continue;
+                };
+                let mac = if let Some(x) = record.get_property_val_str("mac") {
+                    x.to_owned()
+                } else {
+                    String::new()
+                };
+                let mut ip = None;
+                for addr in record.get_addresses().iter() {
+                    if let IpAddr::V4(addr) = addr {
+                        if addr.is_private() {
+                            ip = Some(addr.to_string());
+                            break;
                         }
                     }
-                    _ => {}
+                }
+                let ip = if let Some(ip) = ip { ip } else { continue };
+
+                if let Some(device) = devices.iter_mut().find(|x| x.name == name) {
+                    device.ip = ip;
+                } else {
+                    devices.push(DeviceInfo { name, mac, ip, online: true })
                 }
             }
-            Either::Right(_) => break,
+            ServiceEvent::SearchStopped(_) => {}
+            _ => {}
         }
     }
     devices.sort_by(|a, b| a.name.cmp(&b.name));
